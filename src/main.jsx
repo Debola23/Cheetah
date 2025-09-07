465e403e1454bf831c46d9049b8857be2e8c7730
Solaja Emmanuel <emmanueladebola98@gmail.com>
2025-09-06 17:54:38 -0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,6 @@ import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import { Listing } from './Pages/Listing/Listing.jsx';
 import { Categories } from './Pages/Categories/Categories.jsx';
 import { Contact } from './Pages/Contact/Contact.jsx';
-import './i18n';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n.js';
 import { Favourite } from './Components/Favourite/Favourite.jsx';
 
 
@@ -41,8 +38,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <I18nextProvider i18n={i18n}>
       <RouterProvider router={router} />
-    </I18nextProvider>
   </StrictMode>
 );
